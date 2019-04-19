3f5fb6651841bc756b46facb895ac7b2d8662bfd
Ernest Godlewski <godlewse@pl1mcl-c02yc4vcjg5j.ttg.global>
2019-04-19 11:25:52 +0200

    First commit

---
+++ raywenderlich app/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  raywenderlich app
+//
+//  Created by Ernest Godlewski on 19/04/2019.
+//  Copyright © 2019 Ernest Godlewski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int num = 400;
+        NSLog(@"num equals: %i", num);
+    }
+    return 0;
+}
